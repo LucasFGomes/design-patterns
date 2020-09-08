8ae44f4e1f7b97d2674ddaa94f5c83352c2b09ee
Lucas Ferreira <lucasgomm33@gmail.com>
2020-09-08 18:14:43 -0300

    builder

---
+++ design-patterns/src/builder/NotaFiscalBuilder.java
@@ -0,0 +1,52 @@
+package builder;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
+public class NotaFiscalBuilder {
+	
+	private String razaoSocial;
+	private String cnpj;
+	private List<ItemDaNota> todosItens = new ArrayList<ItemDaNota>();
+	private double valorBruto;
+	private double impostos;
+	private String observacoes;
+	private Calendar dataDaEmissao;
+	
+	public NotaFiscalBuilder() {
+		this.dataDaEmissao = Calendar.getInstance();
+	}
+
+	public NotaFiscalBuilder paraEmpresa(String razaoSocial) {
+		this.razaoSocial = razaoSocial;
+		return this;
+	}
+	
+	public NotaFiscalBuilder comCnpj(String cnpj) {
+		this.cnpj = cnpj;
+		return this;
+	}
+	
+	public NotaFiscalBuilder com(ItemDaNota item) {
+		todosItens.add(item);
+		valorBruto += item.getValor();
+		impostos += item.getValor() * 0.05;
+		return this;
+	}
+	
+	public NotaFiscalBuilder comObservacoes(String observacoes) {
+		this.observacoes = observacoes;
+		return this;
+	}
+	
+	public NotaFiscalBuilder naData(Calendar data) {
+        this.dataDaEmissao = data;
+        return this;
+    }
+	
+	public NotaFiscal constroi() {
+		return new NotaFiscal(razaoSocial, cnpj, dataDaEmissao, valorBruto, impostos, todosItens, observacoes);
+	}
+
+}
